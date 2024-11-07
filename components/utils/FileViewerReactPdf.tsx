2288ee1a1914bf07449a52e61d75bc2afe410143
DikhaAG <dikhaagsfrdy@gmail.com>
2024-11-07 14:16:40 +0700

    ubah react-pdf profider

--- components/utils/FileViewerReactPdf.tsx
+++ components/utils/FileViewerReactPdf.tsx
@@ -14,38 +14,36 @@ const FileViewerReactPdf = ({ file }: FileViewerReactPdfInterface) => {
     setNumPages(numPages);
   }
   return (
-    <FileViewerReactPdfProvider>
-      <div className="flex flex-col">
-        <div className="flex flex-row w-fit mx-auto gap-5 text-2xl text-white">
-          <button
-            onClick={() => setPageNumber(pageNumber - 1)}
-            className={`bg-blue-500 hover:bg-blue-600 hover:scale-105 px-4 py-2 rounded-lg disabled:bg-blue-300`}
-            disabled={pageNumber === 1 ? true : false}
-          >
-            {"<"}
-          </button>
-          <button
-            onClick={() => setPageNumber(pageNumber + 1)}
-            className={`bg-blue-500 hover:bg-blue-600 hover:scale-105 px-4 py-2 rounded-lg disabled:bg-blue-300`}
-            disabled={pageNumber === numPages ? true : false}
-          >
-            {">"}
-          </button>
-        </div>
-        <div className="">
-          <Document
-            file={file}
-            onLoadSuccess={onDocumentLoadSuccess}
-            className={"max-h-screen items-center overflow-x-auto"}
-          >
-            <Page pageNumber={pageNumber} />
-          </Document>
-          <p>
-            Page {pageNumber} of {numPages}
-          </p>
-        </div>
+    <div className="flex flex-col">
+      <div className="flex flex-row w-fit mx-auto gap-5 text-2xl text-white">
+        <button
+          onClick={() => setPageNumber(pageNumber - 1)}
+          className={`bg-blue-500 hover:bg-blue-600 hover:scale-105 px-4 py-2 rounded-lg disabled:bg-blue-300`}
+          disabled={pageNumber === 1 ? true : false}
+        >
+          {"<"}
+        </button>
+        <button
+          onClick={() => setPageNumber(pageNumber + 1)}
+          className={`bg-blue-500 hover:bg-blue-600 hover:scale-105 px-4 py-2 rounded-lg disabled:bg-blue-300`}
+          disabled={pageNumber === numPages ? true : false}
+        >
+          {">"}
+        </button>
       </div>
-    </FileViewerReactPdfProvider>
+      <div className="">
+        <Document
+          file={file}
+          onLoadSuccess={onDocumentLoadSuccess}
+          className={"max-h-screen items-center overflow-x-auto"}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      </div>
+    </div>
   );
 };
 
