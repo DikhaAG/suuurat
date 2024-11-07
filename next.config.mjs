b2d6b43854e8cdfc4f770f218291f2af99630ab5
DikhaAG <dikhaagsfrdy@gmail.com>
2024-11-07 15:08:25 +0700

    downgrade react-pdf version to 8.0.2

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.module.rules.push({
-      test: /\.node/,
-      use: "raw-loader",
-    });
     config.resolve.alias.canvas = false;
-    config.resolve.alias.encoding = false;
+
     return config;
   },
 };
