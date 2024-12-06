8349515366b047225e108e2dd7e98ebff77e2efe
DikhaAG <dikhaagsfrdy@gmail.com>
2024-12-06 13:45:56 +0700

    changing ui

--- app/admin/history/viewer/[suratId]/page.tsx
+++ app/admin/history/viewer/[suratId]/page.tsx
@@ -1,10 +1,13 @@
-"use client"
+"use client";
 import { getSuratById } from "@/app/lib/actions";
 import { SuratModel } from "@/app/lib/models";
 import FileViewerReactPdf from "@/components/utils/FileViewerReactPdf";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
 
 // const AdminHistoryViewerPage = async () => {
 //   const { suratId } = useParams<{ suratId: string }>();
@@ -28,18 +31,18 @@ import { useEffect, useState } from "react";
 
 const AdminHistoryViewerPage = () => {
   const { suratId } = useParams<{ suratId: string }>();
-  const [surat, setSurat] = useState<SuratModel | null>()
+  const [surat, setSurat] = useState<SuratModel | null>();
   useEffect(() => {
-    getSuratById(suratId).then((res) => setSurat(res))
-  }, [])
+    getSuratById(suratId).then((res) => setSurat(res));
+  }, []);
   return (
     <div className="flex flex-col gap-3">
       <div className="flex flex-row w-full">
-        <Link
-          href={"/admin/history"}
-          className="bg-blue-500 rounded-lg px-3 py-2 text-white"
-        >
-          kembali
+        <Link href="/admin/history">
+          <Button variant={"ghost"} className="w-fit">
+            <ChevronLeft />
+            Kembali
+          </Button>
         </Link>
       </div>
       <FileViewerReactPdf file={surat?.file as string} />
