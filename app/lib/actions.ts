9d9aafa5b7a9ae677136cd5bba743f88e46423a9
DikhaAG <dikhaagsfrdy@gmail.com>
2024-12-12 18:42:54 +0700

    some eslint errs

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -447,7 +447,7 @@ export const kirim = async (prevState: unknown, formData: FormData) => {
           authorId,
           receiver,
           file: url,
-          validationStageId: validationStage?.id,
+          validationStageId: validationStage!.id,
         },
       });
     } catch (error) {
