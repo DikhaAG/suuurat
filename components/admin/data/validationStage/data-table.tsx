71b4f3a7591379b10f38723a0a7b9aa8dfe3bec2
DikhaAG <dikhaagsfrdy@gmail.com>
2024-12-14 23:16:15 +0700

    adding feature validator

--- components/admin/data/validationStage/data-table.tsx
+++ components/admin/data/validationStage/data-table.tsx
@@ -70,10 +70,10 @@ export function DataTable<TData, TValue>({
     <>
       <div className="flex items-center py-4">
         <Input
-          placeholder="Filter nama..."
-          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          placeholder="Filter tahap..."
+          value={(table.getColumn("title")?.getFilterValue() as string) ?? ""}
           onChange={(event) =>
-            table.getColumn("name")?.setFilterValue(event.target.value)
+            table.getColumn("title")?.setFilterValue(event.target.value)
           }
           className="max-w-sm"
         />
